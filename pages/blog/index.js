3b044fd4ca025562c12c9a8c1eef01f9c63815a6
Dagim Asnake <dagimasnake1@gmail.com>
2023-05-16 23:45:03 -1200

    connect to database send blog &&  fetch blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,35 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
 const index = () => {
+  const router = useRouter();
+
+  const [title, setTitle] = useState('')
+  const [topic, setTopic] = useState('')
+  const [content, setContent] = useState('')
+
+  const onFormSubmisson = (e) => {
+    e.preventDefault()
+
+    const formData = {
+      title: title,
+      topic: topic,
+      content: content
+    }
+
+    fetch('http://localhost:8000/blog/add', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    router.push('/');
+
+  }
+
   return (
     <>
       <Head>
@@ -8,18 +37,18 @@ const index = () => {
       </Head>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24">
         <h1 className="text-4xl font-bold mb-8">Add a New Blog Post</h1>
-        <form className="max-w-lg mx-auto">
+        <form className="max-w-lg mx-auto" onSubmit={onFormSubmisson}>
           <div className="mb-4">
             <label htmlFor="title" className="block text-gray-700 font-bold mb-2">Title</label>
-            <input type="text" id="title" name="title" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} name="title" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
           <div className="mb-4">
             <label htmlFor="topic" className="block text-gray-700 font-bold mb-2">Main Topic</label>
-            <input type="text" id="topic" name="topic" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <input type="text" id="topic" value={topic} onChange={(e) => setTopic(e.target.value)} name="topic" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
           <div className="mb-4">
             <label htmlFor="content" className="block text-gray-700 font-bold mb-2">Content</label>
-            <textarea id="content" name="content" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="10"></textarea>
+            <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} name="content" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="10"></textarea>
           </div>
           <div className="flex justify-end">
             <button type="submit" className="bg-black text-white hover:bg-white hover:text-black hover:border-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:shadow-md">
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,47 @@
 import Head from 'next/head';
+import React from 'react';
+import moment from 'moment';
 
-const Home = () => {
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:8000/blog')
+  const data = await res.json()
+  const datas = data.msg
+
+  return {
+    props: { datas },
+  }
+}
+
+const Home = ({ datas }) => {
   return (
     <>
       <Head>
         <title>Blogs</title>
       </Head>
-      <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="-my-8 divide-y-2 divide-gray-100">
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-                <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
-                <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                <span class="text-sm text-gray-500">12 Jun 2019</span>
-              </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
-                <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            {datas.map(item => {
+              const formattedDate = moment(item.createdAt).format('MMMM Do YYYY, h:mm:ss a');
+              return (
+                <div key={item._id} className="py-8 flex flex-wrap md:flex-nowrap">
+                  <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                    <span className="font-semibold title-font text-gray-700">firstName lastName</span>
+                    <span className="mt-1 text-gray-500 text-sm">{formattedDate}</span>
+                  </div>
+                  <div className="md:flex-grow">
+                    <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">{item.title}</h2>
+                    <p className="leading-relaxed">{item.topic}</p>
+                    <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
+                      <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
@@ -65,3 +50,4 @@ const Home = () => {
 }
 
 export default Home
+
