b91e04fbb0fc0ac8265236bb1d371ae4f2bafe72
Dagim Asnake <dagimasnake1@gmail.com>
2023-05-17 05:41:50 -1200

    edited next.config.js second

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 120,
+  staticPageGenerationTimeout: 180,
 }
 
 module.exports = nextConfig
